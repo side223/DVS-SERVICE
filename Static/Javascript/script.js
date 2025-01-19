bc825f6bfc38dfe0ecc4e0143877fe1f359891d5
sideSoftware <119924520+side223@users.noreply.github.com>
2025-01-19 18:53:25 +0100

    mk3

--- Static/Javascript/script.js
+++ Static/Javascript/script.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "30vh";
+    document.getElementById("main").style.marginLeft = "30vh";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
